1f60baf4fab253585408c61c94d5a322236f4236
Villela <98665862+abrahamMtzV@users.noreply.github.com>
2023-09-01 01:55:28 -0600

    Se agrega la función consulta por folio
    Se agrega la función "consultaXfolio()", donde se imprimen los datos de la nota que están en ese folio. Si no existe, no imprime nada.

--- Evidencia 1.py
+++ Evidencia 1.py
@@ -12,3 +12,30 @@ notasDict = [(folio, fecha que ingresa usuario, cliente, total a pagar, {folio: 
 '''
 folios = []
 adquiridosFinal ={}
+
+def consultaXfolio():
+    global fechaHoy, folio, fechaUsuario
+    global fecha_str, cliente, montoPagar, adquiridos, costo  # diccionario main
+    global adquiridosFinal
+    global consultaFolio
+
+    global servicio
+
+    while True:
+        try:
+            print("Ingresa el folio a consultar")
+            consultaFolio=int(input("->"))
+        except Exception:
+            print("Ingrese una opcion correcta.")
+
+        for k in notasDict:
+            if k[0]==consultaFolio:
+                print(f"\nFolio: {k[0]}")
+                print(f"Fecha: {k[1]}")
+                print(f"Cliente: {k[2]}")
+                print(f"Monto a pagar: {k[3]}")
+                print("Servicios:")
+                for i in adquiridosFinal[k[0]]:
+                    print(f"\t- {i[0]} ---- ${i[1]}")
+        break
+    menu()
