1a0976024d454fcd518ee944421327ba293eaa05
Villela <98665862+abrahamMtzV@users.noreply.github.com>
2023-09-01 01:52:22 -0600

    Se agregan listas y diccionarios vacios
    Se integran listas vacias y diccionarios vacios junto con la estructura principal donde se almacenan los datos.

--- Evidencia 1.py
+++ Evidencia 1.py
@@ -1,2 +1,14 @@
 import random as r
 from datetime import datetime
+
+fechaHoy = datetime.today()
+
+notasDict = []
+recuperar = []
+'''
+ESTRUCTURA DE notasDict
+
+notasDict = [(folio, fecha que ingresa usuario, cliente, total a pagar, {folio: [(servicio, costo)]})]
+'''
+folios = []
+adquiridosFinal ={}
