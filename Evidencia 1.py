2fb55b6cadbfd36794e13b1b9237cb4367f7115b
Villela <98665862+abrahamMtzV@users.noreply.github.com>
2023-09-01 01:57:31 -0600

    Se agrega función consulta
    Esta función consulta por un rango de fechas, dentro de la lista con los datos generales de todas las notas creadas.

--- Evidencia 1.py
+++ Evidencia 1.py
@@ -39,3 +39,57 @@ def consultaXfolio():
                     print(f"\t- {i[0]} ---- ${i[1]}")
         break
     menu()
+
+
+def consultas():
+    global fechaHoy, folio, fechaUsuario
+    global fecha_str, cliente, montoPagar, adquiridos, costo  # diccionario main
+    global adquiridosFinal
+
+    global servicio
+
+    while True:
+        try:
+            elige = int(input("\n(1) Consulta por folio\n(2) Consulta por rango de fechas.\n->"))
+            if not elige in (1, 2):continue
+            elif elige==1: consultaXfolio()
+        except Exception:
+            print("Ingrese un valor correcto.")
+        break
+
+
+    while True:
+        try:
+            for j in notasDict:
+                # Solicitar las fechas de inicio y fin al usuario
+                inicio_str = input("Ingrese la fecha de inicio en el formato aaaa/mm/dd (Vacio para regresar)\n-> ")
+                if inicio_str == "":
+                    print("Regresando...")
+                    continue
+
+                fin_str = input("Ingrese la fecha de fin en el formato aaaa/mm/dd (Vacio para regresar)\n-> ")
+                if fin_str == "":
+                    print("Regresando...")
+                    continue
+
+                inicio = datetime.strptime(inicio_str, '%Y/%m/%d')
+                fin = datetime.strptime(fin_str, '%Y/%m/%d')
+
+                Existe=False
+                if j[1]>=inicio and j[1]<=fin:
+                    Existe=True
+                    print(f"\nFolio: {j[0]}")
+                    print(f"Fecha: {j[1]}")
+                    print(f"Cliente: {j[2]}")
+                    print(f"Monto a pagar: {j[3]}")
+                    print("Servicios:")
+                    for i in adquiridosFinal[j[0]]:
+                        print(f"\t- {i[0]} ---- ${i[1]}")
+                if not Existe:
+                    print("No existe ningun dato dentro del rango proporcionado.")
+                    continue
+
+
+        except Exception:
+            print("Error al buscar por fechas.")
+        else: break
